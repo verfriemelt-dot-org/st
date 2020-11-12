099d90d7adf72b65bec50e3a67bed0e965744fe9
ಠ_ಠ <easteregg@verfriemelt.org>
2020-11-12 11:00:31 +0100

    xresources

--- config.h
+++ config.h
@@ -53,7 +53,7 @@ int allowwindowops = 0;
  * near minlatency, but it waits longer for slow updates to avoid partial draw.
  * low minlatency will tear/flicker more, as it can "detect" idle too early.
  */
-static double minlatency = 2;
+static double minlatency = 8;
 static double maxlatency = 33;
 
 /*
@@ -123,8 +123,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"black",
 };
 
 
@@ -132,15 +131,9 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-<<<<<<< HEAD
-unsigned int defaultfg = 258;
-unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
-=======
 unsigned int defaultfg = 7;
 unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
->>>>>>> 002ffcc (alpha)
 static unsigned int defaultrcs = 257;
 
 /*
@@ -482,39 +475,40 @@ static char ascii_printable[] =
 	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
 	"`abcdefghijklmnopqrstuvwxyz{|}~";
 
-/*
- * Xresources preferences to load at startup
- */
-ResourcePref resources[] = {
-   { "font",         STRING,  &font },
-   { "color0",       STRING,  &colorname[0] },
-   { "color1",       STRING,  &colorname[1] },
-   { "color2",       STRING,  &colorname[2] },
-   { "color3",       STRING,  &colorname[3] },
-   { "color4",       STRING,  &colorname[4] },
-   { "color5",       STRING,  &colorname[5] },
-   { "color6",       STRING,  &colorname[6] },
-   { "color7",       STRING,  &colorname[7] },
-   { "color8",       STRING,  &colorname[8] },
-   { "color9",       STRING,  &colorname[9] },
-   { "color10",      STRING,  &colorname[10] },
-   { "color11",      STRING,  &colorname[11] },
-   { "color12",      STRING,  &colorname[12] },
-   { "color13",      STRING,  &colorname[13] },
-   { "color14",      STRING,  &colorname[14] },
-   { "color15",      STRING,  &colorname[15] },
-   { "background",   STRING,  &colorname[256] },
-   { "foreground",   STRING,  &colorname[257] },
-   { "cursorColor",  STRING,  &colorname[258] },
-   { "termname",     STRING,  &termname },
-   { "shell",        STRING,  &shell },
-   { "minlatency",   INTEGER, &minlatency },
-   { "maxlatency",   INTEGER, &maxlatency },
-   { "blinktimeout", INTEGER, &blinktimeout },
-   { "bellvolume",   INTEGER, &bellvolume },
-   { "tabspaces",    INTEGER, &tabspaces },
-   { "borderpx",     INTEGER, &borderpx },
-   { "cwscale",      FLOAT,   &cwscale },
-   { "chscale",      FLOAT,   &chscale },
-};
 
+  /*
+   * Xresources preferences to load at startup
+   */
+  ResourcePref resources[] = {
+     { "font",         STRING,  &font },
+     { "color0",       STRING,  &colorname[0] },
+     { "color1",       STRING,  &colorname[1] },
+     { "color2",       STRING,  &colorname[2] },
+     { "color3",       STRING,  &colorname[3] },
+     { "color4",       STRING,  &colorname[4] },
+     { "color5",       STRING,  &colorname[5] },
+     { "color6",       STRING,  &colorname[6] },
+     { "color7",       STRING,  &colorname[7] },
+     { "color8",       STRING,  &colorname[8] },
+     { "color9",       STRING,  &colorname[9] },
+     { "color10",      STRING,  &colorname[10] },
+     { "color11",      STRING,  &colorname[11] },
+     { "color12",      STRING,  &colorname[12] },
+     { "color13",      STRING,  &colorname[13] },
+     { "color14",      STRING,  &colorname[14] },
+     { "color15",      STRING,  &colorname[15] },
+     { "background",   STRING,  &colorname[256] },
+     { "foreground",   STRING,  &colorname[257] },
+     { "cursorColor",  STRING,  &colorname[258] },
+     { "termname",     STRING,  &termname },
+     { "shell",        STRING,  &shell },
+     { "minlatency",   INTEGER, &minlatency },
+     { "maxlatency",   INTEGER, &maxlatency },
+     { "blinktimeout", INTEGER, &blinktimeout },
+     { "bellvolume",   INTEGER, &bellvolume },
+     { "tabspaces",    INTEGER, &tabspaces },
+     { "borderpx",     INTEGER, &borderpx },
+     { "cwscale",      FLOAT,   &cwscale },
+     { "chscale",      FLOAT,   &chscale },
+  };
+  

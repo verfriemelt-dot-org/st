f6f6c79af33ef5811cf19b8b3b5d791faf85a620
ಠ_ಠ <easteregg@verfriemelt.org>
2020-11-12 15:47:26 +0100

    clean

--- config.h
+++ config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
+static char *font = "mono:pixelsize=16:antialias=true:autohint=true";
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -94,36 +94,32 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.95;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
-	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"#262626",
+  "#282828", /* hard contrast: #1d2021 / soft contrast: #32302f */
+  "#cc241d",
+  "#98971a",
+  "#d79921",
+  "#458588",
+  "#b16286",
+  "#689d6a",
+  "#a89984",
+  "#928374",
+  "#fb4934",
+  "#b8bb26",
+  "#fabd2f",
+  "#83a598",
+  "#d3869b",
+  "#8ec07c",
+  "#ebdbb2",
+  [255] = 0,
+  /* more colors can be added after 255 to use with DefaultXX */
+  "#add8e6", /* 256 -> cursor */
+  "#555555", /* 257 -> rev cursor*/
+  "#282828", /* 258 -> bg */
+  "#ebdbb2", /* 259 -> fg */
 };
 
 
@@ -131,11 +127,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
+unsigned int defaultfg = 259;
 unsigned int defaultbg = 258;
 static unsigned int defaultcs = 256;
 static unsigned int defaultrcs = 257;
-
 /*
  * Default shape of cursor
  * 2: Block ("█")
@@ -183,8 +178,8 @@ static MouseShortcut mshortcuts[] = {
 	// { XK_ANY_MOD,           Button4, ttysend,        {.s = "\031"} },
 	// { ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
 	// { XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
-  { ShiftMask,            Button4,        kscrollup,      {.i = 1} },
-  { ShiftMask,            Button5,        kscrolldown,    {.i = 1} },
+  { XK_ANY_MOD,            Button4,        kscrollup,      {.i = 1} },
+  { XK_ANY_MOD,            Button5,        kscrolldown,    {.i = 1} },
 };
 
 /* Internal keyboard shortcuts. */
@@ -207,6 +202,10 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+
+  { TERMMOD,              XK_K,           zoom,           {.f = +1} },
+  { TERMMOD,              XK_J,           zoom,           {.f = -1} },
+
 };
 
 /*
@@ -479,40 +478,34 @@ static char ascii_printable[] =
 	"@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_"
 	"`abcdefghijklmnopqrstuvwxyz{|}~";
 
+ResourcePref resources[] = {
+    { "font",         STRING,  &font },
+    { "color0",       STRING,  &colorname[0] },
+    { "color1",       STRING,  &colorname[1] },
+    { "color2",       STRING,  &colorname[2] },
+    { "color3",       STRING,  &colorname[3] },
+    { "color4",       STRING,  &colorname[4] },
+    { "color5",       STRING,  &colorname[5] },
+    { "color6",       STRING,  &colorname[6] },
+    { "color7",       STRING,  &colorname[7] },
+    { "color8",       STRING,  &colorname[8] },
+    { "color9",       STRING,  &colorname[9] },
+    { "color10",      STRING,  &colorname[10] },
+    { "color11",      STRING,  &colorname[11] },
+    { "color12",      STRING,  &colorname[12] },
+    { "color13",      STRING,  &colorname[13] },
+    { "color14",      STRING,  &colorname[14] },
+    { "color15",      STRING,  &colorname[15] },
+    { "background",   STRING,  &colorname[258] },
+    { "foreground",   STRING,  &colorname[259] },
+    { "cursorColor",  STRING,  &colorname[256] },
+    { "termname",     STRING,  &termname },
+    { "shell",        STRING,  &shell },
+    { "blinktimeout", INTEGER, &blinktimeout },
+    { "bellvolume",   INTEGER, &bellvolume },
+    { "tabspaces",    INTEGER, &tabspaces },
+    { "borderpx",     INTEGER, &borderpx },
+    { "cwscale",      FLOAT,   &cwscale },
+    { "chscale",      FLOAT,   &chscale },
+};
 
-  /*
-   * Xresources preferences to load at startup
-   */
-  ResourcePref resources[] = {
-     { "font",         STRING,  &font },
-     { "color0",       STRING,  &colorname[0] },
-     { "color1",       STRING,  &colorname[1] },
-     { "color2",       STRING,  &colorname[2] },
-     { "color3",       STRING,  &colorname[3] },
-     { "color4",       STRING,  &colorname[4] },
-     { "color5",       STRING,  &colorname[5] },
-     { "color6",       STRING,  &colorname[6] },
-     { "color7",       STRING,  &colorname[7] },
-     { "color8",       STRING,  &colorname[8] },
-     { "color9",       STRING,  &colorname[9] },
-     { "color10",      STRING,  &colorname[10] },
-     { "color11",      STRING,  &colorname[11] },
-     { "color12",      STRING,  &colorname[12] },
-     { "color13",      STRING,  &colorname[13] },
-     { "color14",      STRING,  &colorname[14] },
-     { "color15",      STRING,  &colorname[15] },
-     { "background",   STRING,  &colorname[256] },
-     { "foreground",   STRING,  &colorname[257] },
-     { "cursorColor",  STRING,  &colorname[258] },
-     { "termname",     STRING,  &termname },
-     { "shell",        STRING,  &shell },
-     { "minlatency",   INTEGER, &minlatency },
-     { "maxlatency",   INTEGER, &maxlatency },
-     { "blinktimeout", INTEGER, &blinktimeout },
-     { "bellvolume",   INTEGER, &bellvolume },
-     { "tabspaces",    INTEGER, &tabspaces },
-     { "borderpx",     INTEGER, &borderpx },
-     { "cwscale",      FLOAT,   &cwscale },
-     { "chscale",      FLOAT,   &chscale },
-  };
-  
